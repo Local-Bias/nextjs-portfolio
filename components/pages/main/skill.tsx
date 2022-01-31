21f8f8954b15a53259209a5aa1a7ab8f59b8d324
Ribbit <echo07200503@gmail.com>
2022-01-31 15:33:59 +0900

    💡 スキルの概要紹介のデザインを修正

--- components/pages/main/skill.tsx
+++ components/pages/main/skill.tsx
@@ -1,12 +1,106 @@
 import React, { VFC, VFCX } from 'react';
 import styled from '@emotion/styled';
-import { Glass } from '../../common/glass';
-import { GlassButton } from '../../common/glass-button';
+import { Button } from '../../common/button';
 import Link from 'next/link';
-import { SKILLS } from '../../../contents/skills';
 
 type ContainerProps = Readonly<{}>;
 type Props = ContainerProps & Readonly<{}>;
+
+const Console: VFCX = ({ className }) => (
+  <div {...{ className }}>
+    <div className='header'>
+      <p>C:\WINDOWS\system32\cmd.exe</p>
+    </div>
+    <div className='content'>
+      <div>Microsoft Windows [Version xx.x.xxxxx.xxx]</div>
+      <div>(c) 2022 Ribbit All rights reserved.</div>
+      <div>&nbsp;</div>
+      <div>function main(arg) {'{}'}</div>
+      <div>const main = (arg: string): void {' => {}'}</div>
+      <div>Private Sub(ByRef arg as String)</div>
+      <div>fn main(arg: char) {}</div>
+      <div>&nbsp;</div>
+      <div>
+        C:\users\user&gt;
+        <span className='highlight'>type nul &gt; %userprofile%\Documents\empty.file</span>
+        <span className='highlight console-piriod'>_</span>
+      </div>
+      <div className='padding'></div>
+    </div>
+  </div>
+);
+
+const StyledConsole = styled(Console)`
+  width: 100%;
+  box-shadow: 0 5px 10px -2px #0003, 0 10px 20px -3px #0003, 0 10px 30px -5px #0003;
+  border: 1px solid #0006;
+  padding: 0 !important;
+  border-radius: 8px;
+  overflow: hidden;
+
+  p {
+    padding: 0;
+    margin: 0;
+  }
+  .header {
+    background-color: #fff;
+    width: 100%;
+    padding: 8px 15px;
+    font-size: 10pt;
+    position: relative;
+  }
+  .header:before {
+    content: '―　　□　　×';
+    font-size: 10pt;
+    font-weight: 600;
+    position: absolute;
+    top: 0;
+    right: 12px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  .highlight {
+    color: #5d5;
+  }
+
+  .content {
+    width: 100%;
+    height: 50vh;
+    background-color: black;
+    overflow-y: scroll;
+    color: #bbb;
+    margin: 0;
+    padding: 1em;
+  }
+  .padding {
+    height: 1000px;
+  }
+  .console-piriod {
+    animation-name: console-piriod;
+    animation-duration: 1s;
+    animation-timing-function: ease;
+    animation-iteration-count: infinite;
+    animation-direction: normal;
+    animation-fill-mode: forwards;
+    transition: all 0.4s ease-out;
+  }
+  @keyframes console-piriod {
+    0% {
+      opacity: 0;
+    }
+    49% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
 
 const Component: VFCX<Props> = ({ className }) => (
   <div {...{ className }}>
@@ -17,14 +111,12 @@ const Component: VFCX<Props> = ({ className }) => (
             <h2>Skills and Tools</h2>
           </div>
           <Link href='/skill/'>
-            <GlassButton>Details</GlassButton>
+            <Button>Details</Button>
           </Link>
         </div>
       </div>
-      <div className='items'>
-        {SKILLS.map((skill, i) => (
-          <Glass key={i}>{skill.name}</Glass>
-        ))}
+      <div>
+        <StyledConsole />
       </div>
     </div>
   </div>
@@ -33,6 +125,8 @@ const Component: VFCX<Props> = ({ className }) => (
 const StyledComponent = styled(Component)`
   padding-left: 100px;
   min-height: 100vh;
+  background-color: #f2f6ff;
+  color: #012d;
 
   .contents {
     display: flex;
@@ -40,12 +134,17 @@ const StyledComponent = styled(Component)`
     @media (max-width: 1000px) {
       flex-direction: column;
     }
-    width: 95vw;
+    width: 90vw;
     margin: auto;
     gap: 32px;
 
     > div {
       width: 100%;
+      &:nth-of-type(2) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
     }
   }
 
@@ -67,13 +166,11 @@ const StyledComponent = styled(Component)`
   }
 
   svg {
-    fill: #fffc;
     width: 100px;
   }
 
   h2 {
-    font-size: 3em;
-    color: #fff;
+    font-size: 60px;
     flex: 1 1 auto;
     width: 100%;
     text-align: center;
@@ -84,27 +181,6 @@ const StyledComponent = styled(Component)`
   justify-content: center;
   align-items: center;
   gap: 10vh;
-
-  .items {
-    padding: 5vh 0;
-    flex: 1 1 auto;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    @media (max-width: 1500px) {
-      grid-template-columns: repeat(2, 1fr);
-    }
-    gap: 32px;
-    width: 100%;
-
-    > div {
-      height: 200px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      font-size: 1.3em;
-    }
-  }
 `;
 
 const Container: VFC<ContainerProps> = () => {
